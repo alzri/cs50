d8e4e198544243d0dc8148aa47d33be2dc8d97d9
azrinka <alzrinka1@gmail.com>
2025-10-25 15:07:42 +0200

    added mario

---
+++ mario-less/mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int spaces, int bricks);
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+        int j = height - i - 1;
+        print_row(j, i + 1);
+    }
+}
+
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+
+    printf("\n");
+}
